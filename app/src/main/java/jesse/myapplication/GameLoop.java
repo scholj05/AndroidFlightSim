fc2a523bb12e856b9c7f5532e64dd4b1b88eade3
Steveo5 <stevenbellamy@hotmail.com>
2017-11-14 17:15:41 +1300

    Working on the heightmap

--- app/src/main/java/jesse/myapplication/GameLoop.java
+++ app/src/main/java/jesse/myapplication/GameLoop.java
@@ -2,6 +2,9 @@ package jesse.myapplication;
 
 import android.util.Log;
 
+import Units.Vector2;
+import Units.Vector3;
+
 /**
  * Created by Jesse on 7/11/2017.
  */
@@ -9,10 +12,18 @@ import android.util.Log;
 public class GameLoop extends Thread
 {
     OpenGLView openGLView;
+    private HeightMap heightMap;
 
     public GameLoop(OpenGLView openGLView)
     {
+
         this.openGLView = openGLView;
+        heightMap = new HeightMap();
+
+        Vector3 startPos = new Vector3(0, 0, 0);
+        Vector2 tileSize = new Vector2(100, 100);
+        int colCount = 128, rowCount = 128, minHeight = 0, maxHeight = 1000;
+        heightMap.Generate(startPos, tileSize, colCount, rowCount, minHeight, maxHeight);
     }
 
     @Override
@@ -22,7 +33,7 @@ public class GameLoop extends Thread
         {
             //Log.d("TAG", "GAMELOOP");
             openGLView.requestRender();
-
+            heightMap.draw();
 
             try
             {
